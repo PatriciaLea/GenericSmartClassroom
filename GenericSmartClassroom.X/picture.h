4e91588e9b62c8788cf2d6661cef9cc950d448ed
PatriciaLea <136087654+PatriciaLea@users.noreply.github.com>
2023-06-29 13:30:41 +0200

    Update picture.h

--- GenericSmartClassroom.X/picture.h
+++ GenericSmartClassroom.X/picture.h
@@ -1,10 +1,12 @@
-#ifndef UART_RECEIVE_H
-#define UART_RECEIVE_H
+#ifndef PICTURE_H_
+#define PICTURE_H_
 
-#include <xc.h>
-#include "uartSend.h"
+//extern const uint16_t Bild1[2904];
+//extern const __flash int Bild1[672];
+extern const __flash int newProject[568];
+//extern const __flash int Bild2[2];
+//extern const __flash int Bild3[2];
+//extern const __flash int Bild4[2];
 
 
-void UART_Receive(Type type, uint16_t data);
-
-#endif
+#endif /* PICTURE_H_ */
