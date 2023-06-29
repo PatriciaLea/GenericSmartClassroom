6860e25b01c50b14cb0f5aac74130a40a4c2f9a7
PatriciaLea <136087654+PatriciaLea@users.noreply.github.com>
2023-06-29 19:56:28 +0200

    Update picture.h

--- GenericSmartClassroom.X/picture.h
+++ GenericSmartClassroom.X/picture.h
@@ -2,12 +2,11 @@
 #ifndef PICTURE_H_
 #define PICTURE_H_
 
-//extern const uint16_t Bild1[2904];
-//extern const __flash int Bild1[672];
-extern const __flash int newProject[568];
-//extern const __flash int Bild2[2];
-//extern const __flash int Bild3[2];
-//extern const __flash int Bild4[2];
+
+extern const __flash int lightHigh[568];
+//extern const __flash int lightMedium[578];
+//extern const __flash int lightLow[568];
+//extern const __flash int lightNo[583];
 
 
 #endif /* PICTURE_H_ */
