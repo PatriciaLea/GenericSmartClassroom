ebafdccc22afc03e9dda04a289af6a89e0149499
PatriciaLea <136087654+PatriciaLea@users.noreply.github.com>
2023-06-29 13:57:54 +0200

    Update picture.h

--- GenericSmartClassroom.X/picture.h
+++ GenericSmartClassroom.X/picture.h
@@ -3,7 +3,7 @@
 
 //extern const uint16_t Bild1[2904];
 //extern const __flash int Bild1[672];
-extern const __flash int newProject[568];
+extern const __flash int newProject[1515];
 //extern const __flash int Bild2[2];
 //extern const __flash int Bild3[2];
 //extern const __flash int Bild4[2];
